96b5fbc2bd9194449666b6c2630e35a9e1413290
dell <piyush6348@gmail.com>
2017-01-29 16:07:47 +0530

    Balancing the Brackets

---
+++ Stacks and Queues/Balancing the Brackets.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool checkingTheBrackets(string input)
+{
+    stack<char> stk;
+
+    for(int i=0;i<input.length();i++)
+    {
+        if(input[i]=='(' || input[i]=='[' || input[i]=='{')
+            stk.push(input[i]);
+
+        else if(input[i]==')' || input[i]==']' || input[i]=='}')
+        {
+            char ch=stk.top();
+            if(stk.empty())
+                return false;
+            stk.pop();
+
+            if(!((input[i]==')' && ch=='(') || (input[i]==']' && ch=='[') || (input[i]=='}' && ch=='{')))
+                return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    string input;
+    cin>>input;
+
+    if(checkingTheBrackets(input))
+        cout<<"Balanced";
+    else
+        cout<<"Not Balanced";
+    return 0;
+}
