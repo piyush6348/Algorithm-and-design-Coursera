ba7f09ab3b8b108d6ec700d19558f231c4d70d1f
dell <piyush6348@gmail.com>
2017-02-10 09:53:33 +0530

    add: Binary Tree Questions

--- Trees/Binary Tree 1.cpp
+++ Trees/Binary Tree 1.cpp
@@ -403,12 +403,112 @@ public:
 
             cout<<tem->data<<" "<<leftHeight<<"  "<<rightHeight<<"\n";
 
-            if(rightHeight-leftHeight==1 || leftHeight-rightHeight==1 || leftHeight==rightHeight){}
+            if(rightHeight-leftHeight==1 || leftHeight-rightHeight==1 || leftHeight==rightHeight) {}
             else
-             return false;
+                return false;
 
         }
         return true;
+    }
+
+    void prettyPrint()
+    {
+        if(root==NULL)
+            return;
+        queue<node*> Queue;
+        Queue.push(root);
+        Queue.push(NULL);
+        int levelElementsCount=0;
+        int height=0;
+        while(!Queue.empty())
+        {
+            node* temp=Queue.front();
+            Queue.pop();
+
+            if(temp==NULL)
+            {
+                if(!Queue.empty())
+                {
+                    Queue.push(NULL);
+                    cout<<"\n";
+                    for(int i=1;i<=2*height-1;i++)
+                        cout<<" ";
+                    for(int i=1;i<=levelElementsCount;i++)
+                    {
+                        if(i%2==1)
+                            cout<<"/ ";
+                        else
+                            cout<<"\ ";
+                    }
+                    cout<<"\n";
+                    levelElementsCount=0;
+                }
+            }
+            else
+            {
+                height=depthOfTree(temp);
+                for(int i=1;i<=2*height;i++)
+                    cout<<" ";
+                cout<<temp->data;
+                if(temp->left!=NULL)
+                {
+                 Queue.push(temp->left);
+                 levelElementsCount+=1;
+                }
+                if(temp->right!=NULL)
+                {
+                  Queue.push(temp->right);
+                  levelElementsCount+=1;
+                }
+                cout<<" "<<levelElementsCount;
+            }
+        }
+    }
+
+    int getDiameter(node * temp)
+    {
+        if(temp==NULL)
+            return (0);
+        int leftHeight=depthOfTree(temp->left);
+        int rightHeight=depthOfTree(temp->right);
+
+        int leftDiameter=getDiameter(temp->left);
+        int rightDiameter=getDiameter(temp->right);
+        return max(leftHeight+rightHeight+1,max(leftDiameter,rightDiameter));
+    }
+    int diameter()
+    {
+        return getDiameter(root);
+    }
+
+    int searchData(int* in,int startIndex,int endIndex,int data)
+    {
+        int i=0;
+        for(i=startIndex;i<=endIndex;i++)
+        {
+            if(in[i]==data)
+                break;
+        }
+        return i;
+    }
+    node* buildTree(int* in,int* post,int startIndex,int endIndex,int &pIndex)
+    {
+        if(startIndex>endIndex)
+            return NULL;
+
+        node* temp=new node();
+        temp->data=post[pIndex];
+       /* temp->left=NULL;
+        temp->right=NULL;*/
+        pIndex-=1;
+        if(startIndex==endIndex)
+            return temp;
+
+        int iIndex=searchData(in,startIndex,endIndex,temp->data);
+        temp->right=buildTree(in,post,iIndex+1,endIndex,pIndex);
+        temp->left=buildTree(in,post,startIndex,iIndex-1,pIndex);
+
+        return temp;
     }
     Tree()
     {
@@ -432,7 +532,7 @@ int main()
     tree2.printTree();
     while(true)
     {
-        cout<<"1.) Enter an Element\n";
+        cout<<"\n1.) Enter an Element\n";
         cout<<"2.) Delete Leaf Nodes\n";
         cout<<"3.) Print Tree\n";
         cout<<"4.) Exit\n";
@@ -445,6 +545,9 @@ int main()
         cout<<"11.) isThereAPathSum\n";
         cout<<"12.) checkIfTwoNodesWithSum\n";
         cout<<"13.) Is balanced(depth of the left and right subtrees of every node differ by 1 or less\n";
+        cout<<"14.) Pretty Print\n";
+        cout<<"15.) Diameter\n";
+        cout<<"16.) Create tree from inorder and postorder\n";
         cin>> choice;
         switch(choice)
         {
@@ -491,6 +594,22 @@ int main()
             break;
         case 13:
             cout<<tree.isBalanced()<<"\n";
+            break;
+        case 14:
+            tree.prettyPrint();
+            break;
+        case 15:
+            cout<<tree.diameter()<<"\n";
+            break;
+        case 16:
+            int in[]   = {4, 8, 2, 5, 1, 6, 3, 7};
+            int post[] = {8, 4, 5, 2, 6, 7, 3, 1};
+            int n = sizeof(in)/sizeof(in[0]);
+            int pIndex=n-1;
+            Tree obj;
+            node *x=obj.buildTree(in,post,0,n-1,pIndex);
+            obj.root=x;
+            obj.printTree();
             break;
         }
     }
